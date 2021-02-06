d1858aae0861a7d09889ead699be32f5c059e51c
asaidillaeva <aliia.saidillaeva@iaau.edu.kg>
2021-02-06 23:27:52 +0600

    onSaveInstanceState

--- app/src/main/java/com/example/greeter/MainActivity.kt
+++ app/src/main/java/com/example/greeter/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    var words: String = "Hello, "
     private var nameEditText: EditText? = null
     private var greetTextView: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,13 +21,28 @@ class MainActivity : AppCompatActivity() {
 
     fun onGreetClicked(view: View) {
         if (nameEditText!!.text.toString() != "")
-            greetTextView!!.text = "Hello, " + nameEditText!!.text.toString().capitalizeWords() + "!"
+            words = "Hello, " + nameEditText!!.text.toString().capitalizeWords() + "!"
         else
-            greetTextView!!.text = ""
+            words = "Hello"
             nameEditText!!.setHintTextColor(Color.parseColor("#FF03DAC5"))
 
+        greetTextView!!.text = words
     }
 
     fun String.capitalizeWords(): String = split(" ").map { it.capitalize() }.joinToString(" ")
 
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("words", words)
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        words = savedInstanceState.getString("words").toString()
+        greetTextView!!.text = words
+
+    }
 }
